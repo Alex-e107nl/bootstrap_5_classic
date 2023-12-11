11a7422aeb1357b6ef7cf43162e83e0f38a5aff4
Alex <info@exas.nl>
2023-12-11 12:51:11 +0100

    Added glightbox, 3 menu templates
    For the news i added already the lightbox,, images in news should popup, i have also used it in 2 menu templates so the images used in the fullwidth menu's popup when you click on them.. do not know if that stays :-)

--- templates/menu_template.php
+++ templates/menu_template.php
@@ -1,27 +1,89 @@
 <?php
-if (!defined('e107_INIT')) { exit; }
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|		Theme for e107 build with Bootstrap 5.3.2. by exas.nl
++ ----------------------------------------------------------------------------+
+*/
 
+
+
+
+
+if (!defined('e107_INIT'))
+{
+	exit;
+}
+
+$popupimage = 	"
+					<a href='{CMENUIMAGE=url}' class='glightbox' data-glightbox='title:{CMENUTITLE}'>
+					<img class='img-fluid' src='{CMENUIMAGE=url}' alt='image'></a>
+				";
+		
+	
 #### Panel Template - Used by menu_class.php  for Custom Menu Content.
 #### Additional control over image thumbnailing is possible via SETIMAGE e.g. {SETIMAGE: w=200&h=150&crop=1}
 
-	$MENU_TEMPLATE['default']['start'] 					= '<div class="cpage-menu {CMENUNAME}">';
-	$MENU_TEMPLATE['default']['body'] 					= '{CMENUBODY}'; 
-	$MENU_TEMPLATE['default']['end'] 					= '</div>';
+$MENU_TEMPLATE['default']['start'] 					= '<div class="cpage-menu {CMENUNAME}">';
+$MENU_TEMPLATE['default']['body'] 					= '{CMENUBODY}';
+$MENU_TEMPLATE['default']['end'] 					= '</div>';
 
-	$MENU_TEMPLATE['button']['start'] 					= '<div class="cpage-menu {CMENUNAME}">';
-	$MENU_TEMPLATE['button']['body'] 					= '<div>{CMENUBODY}</div>{CPAGEBUTTON}';
-	$MENU_TEMPLATE['button']['end'] 					= '</div>'; 
+$MENU_TEMPLATE['button']['start'] 					= '<div class="cpage-menu {CMENUNAME}">';
+$MENU_TEMPLATE['button']['body'] 					= '<div>{CMENUBODY}</div>{CPAGEBUTTON}';
+$MENU_TEMPLATE['button']['end'] 					= '</div>';
 
-	$MENU_TEMPLATE['buttom-image']['start'] 			= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
-	$MENU_TEMPLATE['buttom-image']['body'] 				= '<div>{CMENUIMAGE}</div>{CPAGEBUTTON}';
-	$MENU_TEMPLATE['buttom-image']['end'] 				= '</div>';
+$MENU_TEMPLATE['buttom-image']['start'] 			= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
+$MENU_TEMPLATE['buttom-image']['body'] 				= '<div>{CMENUIMAGE}</div>{CPAGEBUTTON}';
+$MENU_TEMPLATE['buttom-image']['end'] 				= '</div>';
 
-	$MENU_TEMPLATE['image-only']['start'] 				= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
-	$MENU_TEMPLATE['image-only']['body'] 				= '{CMENUIMAGE}';
-	$MENU_TEMPLATE['image-only']['end'] 				= '</div>';
+$MENU_TEMPLATE['image-only']['start'] 				= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
+$MENU_TEMPLATE['image-only']['body'] 				= '{CMENUIMAGE}';
+$MENU_TEMPLATE['image-only']['end'] 				= '</div>';
 
-	$MENU_TEMPLATE['image-text-button']['start'] 		= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
-	$MENU_TEMPLATE['image-text-button']['body'] 		= '{CMENUIMAGE}{CMENUBODY}{CPAGEBUTTON}';
-	$MENU_TEMPLATE['image-text-button']['end'] 			= '</div>';
+$MENU_TEMPLATE['image-only-fw']['start'] 				= '<div class="text-center cpage-menu {CMENUNAME}">{SETIMAGE: w=0}';
+$MENU_TEMPLATE['image-only-fw']['body'] 				= '{CMENUIMAGE}';
+$MENU_TEMPLATE['image-only-fw']['end'] 				= '</div>';
+
+$MENU_TEMPLATE['textleft-imageright-fw']['start'] 		= '<div class="row m-0 g-0 fs-5 bg-success-subtle ">{SETIMAGE: w=0}';
+$MENU_TEMPLATE['textleft-imageright-fw']['body'] 		= ' 
+
+							<div class="col-12 ps-lg-3">
+							<span class="col-12 col-lg-6 col-xxl-4 mb-5 mb-lg-0 ms-0 ms-lg-5 float-end">'.$popupimage.'</span>
+							
+							<div class="m-lg-5">
+								<h2 class="mt-3 p-3">  {CMENUICON} {CMENUTITLE}</h2>
+        						<div class="p-3"><p>{CMENUBODY}</p></div>
+								<div class=" px-3 mb-3">{CPAGEBUTTON}</div></div>
+							</div>
+							
+						
+';
+$MENU_TEMPLATE['textleft-imageright-fw']['end'] 		= '</div>';
+
+$MENU_TEMPLATE['imageleft-textright-fw']['start'] 		= '<div class="row m-0 g-0 fs-5 bg-warning-subtle">{SETIMAGE: w=0}';
+$MENU_TEMPLATE['imageleft-textright-fw']['body'] 		= ' 
+							
+							<div class="col-12 pe-lg-3 text-lg-end">
+							<span class="col-12 col-lg-6 col-xxl-4 mb-5 mb-lg-0 me-0 me-lg-5 float-start">'.$popupimage.'</span>
+							<div class="m-lg-5">
+								<h2 class="mt-3 p-3">  {CMENUICON} {CMENUTITLE}</h2>
+        						<div class="p-3"><p>{CMENUBODY}</p></div>
+								<div class=" px-3 mb-3">{CPAGEBUTTON}</div>
+								
+								
+								</div>
+								
+							</div>
+';
+$MENU_TEMPLATE['imageleft-textright-fw']['end'] 		= '</div>';
+
+$MENU_TEMPLATE['textright-imageleft']['start'] 		= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=360}';
+$MENU_TEMPLATE['textright-imageleft']['body'] 		= '{CMENUIMAGE}';
+$MENU_TEMPLATE['textright-imageleft']['end'] 		= '</div>';
 
 
+$MENU_TEMPLATE['image-text-button']['start'] 		= '<div class="cpage-menu {CMENUNAME}">{SETIMAGE: w=0}';
+$MENU_TEMPLATE['image-text-button']['body'] 		= ' <div class="footer-logo">{CMENUIMAGE}</div>
+    <div class="text">{CMENUBODY}{CPAGEBUTTON}</div>';
+$MENU_TEMPLATE['image-text-button']['end'] 			= '</div>';
+ 
---
+++ templates/page_template.php
@@ -0,0 +1,170 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2013 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+*/
+
+if (!defined('e107_INIT'))
+{
+	exit;
+}
+
+$PAGE_WRAPPER = array();
+
+$PAGE_TEMPLATE = array();
+
+global $sc_style;
+
+$pageheader =
+	'<div class="e107-page-header breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 ">
+                <div class="banner-content d-flex flex-column justify-content-center">
+                    <h1 class="page-title">{CPAGETITLE}</h1>
+					{CPAGESUBTITLE}
+                </div>
+                {---BREADCRUMB---}
+            </div>
+        </div>
+    </div>
+</div>';
+
+$PAGE_WRAPPER['default']['CPAGESUBTITLE']   = '<p>{---}</p>';
+$PAGE_WRAPPER['default']['CPAGEMESSAGE']    = '{---}<div class="clear"><!-- --></div>';
+$PAGE_WRAPPER['default']['CPAGEAUTHOR']     = "{---}, ";
+$PAGE_WRAPPER['default']['CPAGENAV']        = '<div class="f-right pull-right float-right col-md-3">{---}</div>';
+
+$PAGE_WRAPPER['2columns'] = $PAGE_WRAPPER['default'];
+$PAGE_WRAPPER['fullwidth'] = $PAGE_WRAPPER['default'];
+
+#### default template - BC ####
+// used only for parsing comment outside of the page tablerender-ed content
+// leave empty if you integrate page comments inside the main page template
+
+
+
+
+
+
+// always used - it's inside the {PAGE} sc from 'page' template
+$PAGE_TEMPLATE['default']['start'] = '<div id="{CPAGESEF}" class="cpage_body cpage-body">{CHAPTER_BREADCRUMB}';
+
+// page body
+$PAGE_TEMPLATE['default']['body'] = '
+		{CPAGEMESSAGE}	
+		<div class="clear"><!-- --></div>
+		{CPAGENAV}
+		{CPAGEBODY}
+		<div class="clear"><!-- --></div>
+		<span class="m-3">{CPAGEEDIT}</span>
+		{CPAGERATING}
+		{PAGECOMMENTS}';
+
+// {CPAGEFIELD: name=image}
+
+$PAGE_WRAPPER['default']['CPAGEEDIT'] = "<div class='text-right text-end'>{---}</div>";
+
+// used only when password authorization is required
+$PAGE_TEMPLATE['default']['authorize'] = '
+		<div class="cpage-restrict ">
+			{message}
+			{form_open}
+			<div class="panel panel-default">
+				<div class="panel-heading">{caption}</div>
+					<div class="panel-body">
+					    <div class="form-group">
+				       		 <label class="col-sm-3 control-label">{label}</label>
+					        <div class="col-sm-9">
+					               {password} {submit} 
+					        </div>
+			     		</div>
+					</div>
+      			</div>
+			{form_close}
+		</div>
+	';
+
+// used when access is denied (restriction by class)
+$PAGE_TEMPLATE['default']['restricted'] = '
+		{text}
+	';
+
+// used when page is not found
+$PAGE_TEMPLATE['default']['notfound'] = '
+		{text}
+	';
+
+// always used
+$PAGE_TEMPLATE['default']['end'] = '{CPAGERELATED: types=page,news}</div>';
+ 
+// options per template - disable table render
+//	$PAGE_TEMPLATE['default']['noTableRender'] = false; //XXX Deprecated
+
+// define different tablerender mode here
+$PAGE_TEMPLATE['default']['tableRender'] = 'cpage';
+
+$PAGE_TEMPLATE['default']['related']['caption'] = '{LAN=RELATED}';
+$PAGE_TEMPLATE['default']['related']['start'] = '{SETIMAGE: w=350&h=350&crop=1}<div class="row">';
+$PAGE_TEMPLATE['default']['related']['item'] = '<div class="col-md-4"><a href="{RELATED_URL}">{RELATED_IMAGE}</a><h3><a href="{RELATED_URL}">{RELATED_TITLE}</a></h3></div>';
+$PAGE_TEMPLATE['default']['related']['end'] = '</div>';
+
+// $PAGE_TEMPLATE['default']['editor'] = '<ul class="fa-ul"><li><i class="fa fa-li fa-edit"></i> Level 1</li><li><i class="fa fa-li fa-cog"></i> Level 2</li></ul>';
+
+
+#### No table render example template ####
+
+/*******************  END OF DEFAULT TEMPLATE *******************/
+
+/* DEFAULT PAGE LAYOUT */
+$PAGE_TEMPLATE['default']['page'] = $pageheader . '<div class="container my-5">{PAGE}</div>';
+
+/* FULLWIDTH PAGE LAYOUT */
+$PAGE_TEMPLATE['fullwidth'] =  $PAGE_TEMPLATE['default'];
+$PAGE_TEMPLATE['fullwidth']['page'] = $pageheader . '<div class="container-fluid my-5">{PAGE}</div>';
+
+/*  BARE PAGE LAYOUT - no additional HTML markup - used custom key to override core one */
+$PAGE_TEMPLATE['custom'] =  $PAGE_TEMPLATE['default'];
+$PAGE_TEMPLATE['custom']['page'] = '
+{PAGE}';
+$PAGE_TEMPLATE['custom']['body'] = '<style>.cpage_body { padding-top: 100px; } </style>  {CPAGEBODY}';
+
+/* 2 COLUMNS LAYOUT With menu */
+$PAGE_TEMPLATE['2columns'] =  $PAGE_TEMPLATE['default'];
+$PAGE_TEMPLATE['2columns']['page'] = $pageheader . '<div class="container my-5">{PAGE}</div>';
+
+$PAGE_TEMPLATE['2columns']['body'] = '
+	<div class="row"> 
+		<div class="col-md-6"><h2 class="my-2">{CPAGETITLE}</h2><h3 class="my-2">{CPAGESUBTITLE}</h3>
+		{CPAGEBODY}
+		</div>
+		<div class="col-md-6"><h3 class="my-2">{CMENUTITLE}</h3>
+		{CMENUBODY}
+		</div>
+		<span class="m-3">{CPAGEEDIT}</span>
+	</div>';
+
+
+/* how to rid this key? */
+$PAGE_WRAPPER['profile']['CMENUIMAGE: template=profile'] = '<span class="page-profile-image pull-left col-xs-12 col-sm-4 col-md-4">{---}</span>';
+$PAGE_TEMPLATE['profile'] = $PAGE_TEMPLATE['default'];
+$PAGE_TEMPLATE['profile']['body'] = '
+		{CPAGEMESSAGE}
+		{CPAGESUBTITLE}
+		<div class="clear"><!-- --></div>
+
+		{CPAGENAV|default}
+		{SETIMAGE: w=320}
+		{CMENUIMAGE: template=profile}
+		{CPAGEBODY}
+
+		<div class="clear"><!-- --></div>
+		{CPAGERATING}
+		{CPAGEEDIT}
+	';
+
+ 
