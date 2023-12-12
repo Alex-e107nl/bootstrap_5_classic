b01939eb0854f5b0c2aadeb82182b3ac5d12da4d
Alex <info@exas.nl>
2023-12-12 16:24:32 +0100

    News grit customcards 2
    Made a newsgrid customcard2 like the custom cards on https://getbootstrap.com/docs/5.3/examples/features/ THX @Jimmi08 for the newsimage!

--- templates/news/news_grid_template.php
+++ templates/news/news_grid_template.php
@@ -184,51 +184,46 @@
 	
 	$NEWS_GRID_TEMPLATE['customcard2']['start'] = '<div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">';
 
-	$NEWS_GRID_TEMPLATE['customcard2']['featured'] = '	{SETIMAGE: w=800&h=600&crop=1}
-													<div class="col">
-														<div class="card shadow-sm">
-															{NEWSTHUMBNAIL=placeholder}
-															<div class="card-body">
-																<h3>{NEWSTITLE}</h3>
-																<p>{NEWS_SUMMARY: limit=80}</p>
-																<div class="d-flex justify-content-between align-items-center">
-																	<div class="btn-group">
-																		<a class="btn btn-sm btn-primary" href="{NEWSURL}">{LAN=READ_MORE}</a>
-																		<div class="btn btn-sm btn-outline-secondary">{NEWSCATEGORY}</div>
-               														</div>
-																	<div class="btn btn-sm btn-light"> {NEWSAUTHOR}</div>
-            
-																</div>
+	$NEWS_GRID_TEMPLATE['customcard2']['featured'] = '{SETIMAGE: w=800&h=600&crop=1}
+													<div class="col customcard2b">
+														<div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url({NEWS_IMAGE: type=src});">		
+															<div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+																<h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold"><a class="link-light" href="{NEWS_URL}">{NEWS_TITLE}</a></h3>
+																<ul class="d-flex list-unstyled mt-auto">
+																	<li class="me-auto link-light">
+																		{NEWS_AUTHOR_AVATAR: class=rounded-circle me-3&w=40&h=40&crop=1&placeholder=1} <div class="customcard2 btn btn-sm btn-outline-light"> {NEWSAUTHOR}</div>
+																	</li>
+																	<li class="d-flex align-items-center me-3">
+																		<div class="customcard2 btn btn-sm btn-outline-light">{NEWSCATEGORY}</div>
+																	</li>
+																	<li class="d-flex align-items-center">
+																		<small>{NEWS_DATE=short}</small>
+																	</li>
+																</ul>
 															</div>
 														</div>
 													</div>
             							          ';
 
 	$NEWS_GRID_TEMPLATE['customcard2']['item'] = '{SETIMAGE: w=800&h=600&crop=1}
-													<div class="col">
-														 <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" 
-														 style="background-image: url({NEWS_IMAGE: type=src});">
-														 
-														
-															<div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-																<h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{NEWSTITLE}</h3>
-															
-															<ul class="d-flex list-unstyled mt-auto">
-              <li class="me-auto">
-                <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white">
-              </li>
-              <li class="d-flex align-items-center me-3">
-                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#geo-fill"/></svg>
-                <small>Earth</small>
-              </li>
-              <li class="d-flex align-items-center">
-                <svg class="bi me-2" width="1em" height="1em"><use xlink:href="#calendar3"/></svg>
-                <small>3d</small>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+													<div class="col customcard2b">
+														<div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url({NEWS_IMAGE: type=src});">		
+															<div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+																<h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold"><a class="link-light" href="{NEWS_URL}">{NEWS_TITLE}</a></h3>
+																<ul class="d-flex list-unstyled mt-auto">
+																	<li class="me-auto link-light">
+																		{NEWS_AUTHOR_AVATAR: class=rounded-circle me-3&w=40&h=40&crop=1&placeholder=1} <div class="customcard2 btn btn-sm btn-outline-light"> {NEWSAUTHOR}</div>
+																	</li>
+																	<li class="d-flex align-items-center me-3">
+																		<div class="customcard2 btn btn-sm btn-outline-light">{NEWSCATEGORY}</div>
+																	</li>
+																	<li class="d-flex align-items-center">
+																		<small>{NEWS_DATE=short}</small>
+																	</li>
+																</ul>
+															</div>
+														</div>
+													</div>
 	  
 	  
 	';
